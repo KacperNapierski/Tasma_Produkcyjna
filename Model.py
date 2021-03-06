ddfdfdef3e3370b240f50251d548f6b295d53fd7
KacperNapierski <kacper.t.napierski@gmail.com>
2021-03-06 16:39:59 +0100

    Proba odpowiedzi skokowej (nieudana)

--- Model.py
+++ Model.py
@@ -1,23 +1,33 @@
 from scipy import signal
+from scipy import integrate as spi
+from matplotlib import pyplot as plt
+from numpy import min
+from scipy import linspace
+
 
 # Dane katalogowe
 J_obiektu = 9.82*(10**(-7))
 Kt = 0.0146
-Ob = 0.0
+Ob = 20.0
 
-# nastawy
+# Nastawy
 Predkosc_Kp = 0.01
 Predkosc_Ki = 0.125
 Polozenie_Kp = 60.0
 
 # Wybrowadzenia
-num_predkosc = [-0.005*Ob, Predkosc_Kp*Kt-Ob, Predkosc_Ki*Kt]
-den_predkosc = [J_obiektu*0.005, J_obiektu -0.005*Ob, Predkosc_Kp*Kt-Ob, Predkosc_Ki*Kt]
+#num_predkosc = [-0.005*Ob, Predkosc_Kp*Kt-Ob, Predkosc_Ki*Kt]
+#den_predkosc = [J_obiektu*0.005, J_obiektu -0.005*Ob, Predkosc_Kp*Kt-Ob, Predkosc_Ki*Kt]
+#G_predkosc=signal.TransferFunction(num_predkosc, den_predkosc)
 
-G_predkosc=signal.TransferFunction(num_predkosc, den_predkosc)
-
-#num = [Polozenie_Kp*G_predkosc]
-#den = [1, Polozenie_Kp*G_predkosc]
 num=[-0.005*Polozenie_Kp*Ob, Polozenie_Kp*(Kt*Predkosc_Kp-Ob), Predkosc_Ki*Kt]
 den=[0.005*J_obiektu, J_obiektu-0.005*Ob, Predkosc_Kp*Kt-Ob-0.005*Polozenie_Kp*Ob, Predkosc_Ki*Kt+Predkosc_Kp*Kt-Ob*Polozenie_Kp, Predkosc_Ki*Kt]
 Gc = signal.TransferFunction(num, den)
+#Gc= signal.lti(num, den)
+
+
+#step responce
+t, y = signal.step2(Gc)
+plt.plot(t, y)
+plt.grid()
+plt.show()
